44c669ced4161ed4132b58c163745aa71e52cf16
Eduardo Farófia Medeiros <Edfame@users.noreply.github.com>
2019-04-26 15:51:56 +0100

    Updated programa.c
    Created functions with easy to decipher names so the code is more readable.

--- code/trabalho Final/programa.c
+++ code/trabalho Final/programa.c
@@ -40,8 +40,158 @@ struct processo *novoProcesso(int instante, int codigo[N_MAXIMO_DE_INSTRUCOES * 
     return processo;
 }
 
+
+
+void limparExit(int *processo_em_exit, int *n_processos_corridos, struct processo *processos[]) {
+
+    if (*processo_em_exit != -1) {
+            processos[*processo_em_exit]->pcb->estado = -2;
+            //printf("id: %d mudei no instante %d para lixo\n", processos[processo_em_exit]->pcb->id, timer);
+            (*processo_em_exit) = -1;
+            (*n_processos_corridos)++;
+    }
+
+}
+
+void blockParaWait(queue *block, queue *wait, struct processo *processos[]) {
+
+    if (!isEmpty(block)) {
+        if (block->first <= block->last) {
+            for (int i = block->first; i <= block->last; i++) {
+                processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
+                if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
+    //                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
+    //                        timer);
+                    processos[block->Q[i]]->pcb->estado = 1;
+                    enqueue(dequeue(block), wait);
+                }
+            }
+        } else {
+            for (int i = block->first; i <= block->size; i++) {
+                processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
+                if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
+    //                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
+    //                        timer);
+                    processos[block->Q[i]]->pcb->estado = 1;
+                    enqueue(dequeue(block), wait);
+                }
+            }
+            for (int i = 0; i <= block->last; i++) {
+                processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
+                if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
+    //                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
+    //                        timer);
+                    processos[block->Q[i]]->pcb->estado = 1;
+                    enqueue(dequeue(block), wait);
+                }
+            }
+        }
+    }    
+}
+
+void runParaBlock(int *processo_em_run, queue *block, struct processo *processos[]) {
+
+    processos[*processo_em_run]->pcb->pc++;
+//                printf("id: %d mudei no instante %d para block\n", processos[processo_em_run]->pcb->id, timer);
+    processos[*processo_em_run]->pcb->estado = 3;
+    processos[*processo_em_run]->tempo_que_precisa_de_ficar_em_block = 3;
+    enqueue(*processo_em_run, block);
+    (*processo_em_run) = -1;
+
+}
+
+void runParaExit(int *processo_em_run, int *processo_em_exit, queue *block, struct processo *processos[]) {
+
+    processos[*processo_em_run]->pcb->pc++;
+//                printf("id: %d mudei no instante %d para exit\n", processos[processo_em_run]->pcb->id, timer);
+    processos[*processo_em_run]->pcb->estado = 4;
+    (*processo_em_exit) = (*processo_em_run);
+    enqueue(*processo_em_run, block);
+    (*processo_em_run) = -1;
+
+}
+
+void runParaWait(int *processo_em_run, queue *wait, struct processo *processos[]) {
+
+    processos[*processo_em_run]->pcb->estado = 1;
+    enqueue(*processo_em_run, wait);
+//                printf("id: %d mudei no instante %d para wait\n", processos[processo_em_run]->pcb->id, timer);
+    (*processo_em_run) = -1;   
+
+}
+
+void newParaWait(int p_id, queue *wait, struct processo *processos[]) {
+
+    int n_p = 0;
+    while ((!isFull(wait)) && (n_p < p_id)) {
+
+        if (processos[n_p]->pcb->estado == 0) {
+
+            processos[n_p]->pcb->estado = 1;
+            enqueue(n_p, wait);
+//                  printf("id: %d mudei no instante %d para wait\n", processos[n_p]->pcb->id, timer);
+        }
+        n_p++;
+    }
+}
+
+void waitParaRun(int *processo_em_run, queue *wait, struct processo *processos[]) {
+
+    if (((*processo_em_run) == -1) && (!isEmpty(wait))) {
+
+        (*processo_em_run) = dequeue(wait);
+        processos[*processo_em_run]->pcb->estado = 2; // processo passa para run
+        processos[*processo_em_run]->quantum = 4;
+//            printf("id: %d mudei no instante %d para run\n", processo_em_run, timer);
+    }
+}
+
+void receberParaNew(int timer, int p_id, struct processo *processos[]) {
+
+    for (int n_p = 0; n_p < p_id; n_p++) {
+
+        if (processos[n_p]->instante == timer) {
+            processos[n_p]->pcb->estado = 0;
+//                printf("id: %d mudei no instante %d para new\n", processos[n_p]->pcb->id, timer);
+        }
+    }
+}
+
+void printEstados(int timer, int p_id, struct processo *processos[]) {
+    printf("T: %3d | ", timer);
+
+//print de todos os processos introduzidos
+    for (int n_p = 0; n_p < p_id; n_p++) {
+        switch (processos[n_p]->pcb->estado) {
+            case -2:
+                printf("      | ");
+                break;
+            case 0:
+                printf("NEW   | ");
+                break;
+            case 1:
+                printf("WAIT  | ");
+                break;
+            case 2:
+                printf("RUN   | ");
+                break;
+            case 3:
+                printf("BLOCK | ");
+                break;
+            case 4:
+                printf("EXIT  | ");
+                break;
+            default:
+                break;
+        }
+
+    }
+    printf("\n");
+}
+
 int main(void) {
-    int instante, p_id = 0, controlo;
+
+    int instante, p_id = 0, controlo, n_processos_corridos = 0, timer = 0, processo_em_run = -1, processo_em_exit = -1;
     struct processo *processos[N_MAXIMO_DE_PROCESSOS] = {0};
     char test;
 
@@ -72,81 +222,35 @@ int main(void) {
     queue *wait = new_queue();  // fila do wait
     queue *block = new_queue();  // fila do block
 
-    int n_processos_corridos = 0, timer = 0, processo_em_run = -1, processo_em_exit = -1;
-
     while (1) {
 
-//        printf("\ttime: %d\n", timer);
+        //printf("\ttime: %d\n", timer);
         if (n_processos_corridos == p_id)
             break;
 
-        int n_p;
-
-
         // exit
-        if (processo_em_exit != -1) {
-            processos[processo_em_exit]->pcb->estado = -2;
-//            printf("id: %d mudei no instante %d para lixo\n", processos[processo_em_exit]->pcb->id, timer);
-            processo_em_exit = -1;
-            n_processos_corridos++;
-        }
+        limparExit(&processo_em_exit, &n_processos_corridos, processos);        
 
         // block -> wait
         // nao tem restrico para espço
         // block passa para wait direto
-        if (!isEmpty(block)) {
-            if (block->first <= block->last) {
-                for (int i = block->first; i <= block->last; i++) {
-                    processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
-                    if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
-//                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
-//                        timer);
-                        processos[block->Q[i]]->pcb->estado = 1;
-                        enqueue(dequeue(block), wait);
-                    }
-                }
-            } else {
-                for (int i = block->first; i <= block->size; i++) {
-                    processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
-                    if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
-//                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
-//                        timer);
-                        processos[block->Q[i]]->pcb->estado = 1;
-                        enqueue(dequeue(block), wait);
-                    }
-                }
-                for (int i = 0; i <= block->last; i++) {
-                    processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block--;
-                    if (processos[block->Q[i]]->tempo_que_precisa_de_ficar_em_block == 0) {
-//                        printf("id: %d mudei no instante %d para wait de block\n", processos[block->Q[i]]->pcb->id,
-//                        timer);
-                        processos[block->Q[i]]->pcb->estado = 1;
-                        enqueue(dequeue(block), wait);
-                    }
-                }
-            }
-        }
+        blockParaWait(block, wait, processos);
 
 
         // run -> block se for necessario ou run -> exit
         // run -> wait verificar se atingiu o quantum Q=4 (max) se Q=0 passa para wait
         if (processo_em_run != -1) {
-            if ((processos[processo_em_run]->codigo[processos[processo_em_run]->pcb->pc * 3] == 8) ||
-                (processos[processo_em_run]->codigo[processos[processo_em_run]->pcb->pc * 3] == 9)) {
-                processos[processo_em_run]->pcb->pc++;
-//                printf("id: %d mudei no instante %d para block\n", processos[processo_em_run]->pcb->id, timer);
-                processos[processo_em_run]->pcb->estado = 3;
-                processos[processo_em_run]->tempo_que_precisa_de_ficar_em_block = 3;
-                enqueue(processo_em_run, block);
-                processo_em_run = -1;
 
-            } else if (processos[processo_em_run]->codigo[processos[processo_em_run]->pcb->pc * 3] == 11) {
-                processos[processo_em_run]->pcb->pc++;
-//                printf("id: %d mudei no instante %d para exit\n", processos[processo_em_run]->pcb->id, timer);
-                processos[processo_em_run]->pcb->estado = 4;
-                processo_em_exit = processo_em_run;
-                enqueue(processo_em_run, block);
-                processo_em_run = -1;
+            //depois podemos fazer um switch das inst
+            int inst = processos[processo_em_run]->codigo[processos[processo_em_run]->pcb->pc * 3];
+
+            if (inst == 8 || inst == 9) {
+                
+                runParaBlock(&processo_em_run, block, processos);
+
+            } else if (inst == 11) {
+                
+                runParaExit(&processo_em_run, &processo_em_exit, block, processos);
             }
 
             if (processo_em_run != -1)
@@ -155,76 +259,31 @@ int main(void) {
             processos[processo_em_run]->quantum--;
 
             if (processos[processo_em_run]->quantum == 0) {
-                processos[processo_em_run]->pcb->estado = 1;
-                enqueue(processo_em_run, wait);
-//                printf("id: %d mudei no instante %d para wait\n", processos[processo_em_run]->pcb->id, timer);
-                processo_em_run = -1;
+                
+                runParaWait(&processo_em_run, wait, processos);
             }
         }
 
         // new -> wait
         // posteriormente verificar se tem memoria para o fazer
-        n_p = 0;
-        while ((!isFull(wait)) && (n_p < p_id)) {
-            if (processos[n_p]->pcb->estado == 0) {
-                processos[n_p]->pcb->estado = 1;
-                enqueue(n_p, wait);
-//                printf("id: %d mudei no instante %d para wait\n", processos[n_p]->pcb->id, timer);
-            }
-            n_p++;
-        }
-
+        newParaWait(p_id, wait, processos);
+        
         // wait -> run
-        if ((processo_em_run == -1) && (!isEmpty(wait))) {
-            processo_em_run = dequeue(wait);
-            processos[processo_em_run]->pcb->estado = 2; // processo passa para run
-            processos[processo_em_run]->quantum = 4;
-//            printf("id: %d mudei no instante %d para run\n", processo_em_run, timer);
-        }
-
+        waitParaRun(&processo_em_run, wait, processos);
+        
         // receber o programa no seu instante passando para new
         // posteriormente ver como organizar os arrays devido aos forks
-        for (n_p = 0; n_p < p_id; n_p++) {
-            if (processos[n_p]->instante == timer) {
-                processos[n_p]->pcb->estado = 0;
-//                printf("id: %d mudei no instante %d para new\n", processos[n_p]->pcb->id, timer);
-            }
-        }
-
-        printf("T: %3d | ", timer);
-
-        //print de todos os processos introduzidos
-        for (int n_p = 0; n_p < p_id; n_p++) {
-            switch (processos[n_p]->pcb->estado) {
-                case -2:
-                    printf("      | ");
-                    break;
-                case 0:
-                    printf("NEW   | ");
-                    break;
-                case 1:
-                    printf("WAIT  | ");
-                    break;
-                case 2:
-                    printf("RUN   | ");
-                    break;
-                case 3:
-                    printf("BLOCK | ");
-                    break;
-                case 4:
-                    printf("EXIT  | ");
-                    break;
-                default:
-                    break;
-            }
-
-        }
-        printf("\n");
+        receberParaNew(timer, p_id, processos);
+        
+        //print do instante e do estado de cada processo que já foi introduzido
+        printEstados(timer, p_id, processos);
+        
         timer++;
 
     }
 
     //print de todos os processos introduzidos
+    printf("\nPROCESSOS\n");
     int i = 0;
     struct processo *aux;
     while (i < p_id) {
